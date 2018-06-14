fb4f00bbafcd4305d655e13bf39062b3dc6a9b41
Cesar Castillo Moreno <cesar.cast.more@gmail.com>
2018-06-13 20:25:09 -0500

    agregando google plus cuando ya se inicio sesion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,7 @@ import { GooglePlus } from '@ionic-native/google-plus';
 import firebase from 'firebase';
 
 //https://angularfirebase.com/lessons/ionic-google-login-with-firebase-and-angularfire/
-
+//https://medium.com/@salonimalhotra1ind/ionic-google-sign-in-with-firebase-5d10282cc78
 
 @Component({
   selector: 'page-home',
@@ -20,6 +20,10 @@ import firebase from 'firebase';
 export class HomePage {
 
   public userForm: FormGroup;
+
+  private loading = this.loadingCtrl.create({
+    content: 'Iniciando sesion'
+  });
 
   constructor(private authService: AuthService,
     private loadingCtrl: LoadingController,
@@ -45,18 +49,15 @@ export class HomePage {
 
   onSignin() {
 
-    const loading = this.loadingCtrl.create({
-      content: 'Iniciando sesion'
-    });
 
-    loading.present();
+    this.loading.present();
 
     this.authService.signin(this.userForm.value.email, this.userForm.value.password)
       .then(data => {
-        loading.dismiss();
+        this.loading.dismiss();
       })
       .catch(error => {
-        loading.dismiss();
+        this.loading.dismiss();
 
         const alert = this.alertCtrl.create({
           title: 'Signin failed!',
@@ -70,18 +71,16 @@ export class HomePage {
 
 
   public loginFacebook() {
-    const loading = this.loadingCtrl.create({
-      content: 'Iniciando sesion'
-    });
 
-    loading.present();
+
+    this.loading.present();
 
     this.facebookLogin()
       .then(data => {
-        loading.dismiss();
+        this.loading.dismiss();
       })
       .catch(error => {
-        loading.dismiss();
+        this.loading.dismiss();
 
         const alert = this.alertCtrl.create({
           title: 'Signin failed!',
@@ -94,33 +93,67 @@ export class HomePage {
   }
 
   public loginGoogle() {
-    const loading = this.loadingCtrl.create({
-      content: 'Iniciando sesion'
-    });
 
-    loading.present();
+    this.loading.present();
 
-    this.gplus.login({}).then(res => {
-        loading.dismiss();
+    this.gplus.login({
+        'webClientId': '627434895894-agnmqvlptc9f5r84uh3indcf40puua60.apps.googleusercontent.com',
+        'offline': false,
+        'scopes': 'profile email'
+      }).then(response => {
+        this.loading.dismiss();
+
+        console.log("entrando aqui");
+
+        const googlePlusCredential = firebase.auth.GoogleAuthProvider
+          .credential(response.idToken);
+
+        firebase.auth().signInWithCredential(googlePlusCredential).then(success => {
+          console.log("Firebase success: " + JSON.stringify(success));
+          const alert = this.alertCtrl.create({
+            title: 'Ha entrado',
+            message: 'entrooo',
+            buttons: ['Ok']
+          });
+          alert.present();
+        }).catch(error=>{
+           const alert = this.alertCtrl.create({
+            title: 'Ha error',
+            message: error,
+            buttons: ['Ok']
+          });
+          alert.present();
+        });
+
+
 
       })
-    .catch(err => console.error(err));
-    }
+      .catch(err => {
+        const alert = this.alertCtrl.create({
+          title: 'Signin failed!',
+          message: err,
+          buttons: ['Ok']
+        });
+        alert.present();
+        
 
-
-
-    facebookLogin(): Promise < any > {
-      return this.facebook.login(['email'])
-        .then(response => {
-          const facebookCredential = firebase.auth.FacebookAuthProvider
-            .credential(response.authResponse.accessToken);
-
-          firebase.auth().signInWithCredential(facebookCredential)
-            .then(success => {
-              console.log("Firebase success: " + JSON.stringify(success));
-            });
-
-        }).catch((error) => { console.log(error) });
-    }
-
+      });
   }
+
+
+
+  facebookLogin(): Promise < any > {
+    return this.facebook.login(['email'])
+      .then(response => {
+
+        const facebookCredential = firebase.auth.FacebookAuthProvider
+          .credential(response.authResponse.accessToken);
+
+        firebase.auth().signInWithCredential(facebookCredential).then(success => {
+          console.log("Firebase success: " + JSON.stringify(success));
+        });
+
+      }).catch((error) => { console.log(error) });
+  }
+
+}
